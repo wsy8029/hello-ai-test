ab8e6859b57d85ad2f778dd9159686a1982293f7
wsy8029 <wsy8029@gmail.com>
2020-10-07 14:36:10 +0900

    Implement Tester

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 import cv2
+import sounddevice as sd
+import soundfile as sf
 
-cap = cv2.VideoCapture(0)
+
+data, samplerate = sf.read("./beep.wav")
+
+for idx, device in enumerate(sd.query_devices()):
+    print(device)
+    if 'USB PnP Sound Device' in device['name']:
+        sd.default.device = idx
+        break
+
+
+myrecording = sd.playrec(data, samplerate, channels=1)
+sd.wait()
+sd.play(myrecording, samplerate)
+sd.wait()
+
+cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
 
